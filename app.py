c36e712052a867a72d2317a151f016eb729faa16
Irene Burresi <burresi.irene@icloud.com>
2022-10-20 16:31:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -130,8 +130,8 @@ if selected == "Sales predictor":
 elif selected == "Data Visualisation":
     st.write("### Pandas Profile")
     st.write("---")
-    #st.write(source_code, unsafe_allow_html=True)
-    components.html(source_code, height = 800)
+    st.markdown(source_code, unsafe_allow_html=True)
+    #components.html(source_code, height = 600)
 
 
 
